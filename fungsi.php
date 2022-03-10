7e4c2b04dc9c3ce90c2b318f0951a60e4bce309b
UswatunChasanah <uswatun41100.pesonaxxi@gmail.com>
2022-03-10 20:11:30 +0700

    menambahkan fungsi nama

--- fungsi.php
+++ fungsi.php
@@ -1,1 +1,10 @@
-<?php  ?>
+<?php
+	function nama($nama){
+		echo "Hallo, Selamat datang ".$nama;
+	} //function mengucapkan selamat datang
+	echo nama("Uswatun");
+	echo "<br>";
+	echo nama("Chasanah");
+	echo "<br>";
+	echo nama("Nina");
+?>
