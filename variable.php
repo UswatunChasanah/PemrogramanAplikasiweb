fde06145f40011501790aee492d8667f21703081
UswatunChasanah <uswatun41100.pesonaxxi@gmail.com>
2022-03-10 11:32:52 +0700

    menambahkan operasi penjumlahan, pengurangan, perkalian

---
+++ variable.php
@@ -0,0 +1,12 @@
+<?php 
+	$x = 3;
+	$y = 5;
+	echo "Hasil dari ".$x." + ".$y." = ";
+	echo $x + $y; //operasi penjumlahan
+	echo "<br>";
+	echo "Hasil dari ".$x." - ".$y." = ";
+	echo $x - $y; //operasi pengurangan
+	echo "<br>";
+	echo "Hasil dari ".$x." * ".$y." = ";
+	echo $x * $y; //operasi perkalian
+?>
